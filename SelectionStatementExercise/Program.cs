b55676efe4659d726fa095ad5e374b2277f94c4d
taylor Schuchter <taylorschuchter@gmail.com>
2023-01-24 22:00:15 -0500

    exercise one and two completed

--- SelectionStatementExercise/Program.cs
+++ SelectionStatementExercise/Program.cs
@@ -1,10 +1,60 @@
-﻿namespace SelectionStatementExercise
+﻿using System.Runtime.CompilerServices;
+
+namespace SelectionStatementExercise
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //EXERCISE ONE
+            var favNum = 23;
+            Console.WriteLine("Can you guess my favorite number? Guess a number between 0 and 100!");
+            var userGuess = int.Parse(Console.ReadLine());
+            if (userGuess == favNum) 
+
+            { 
+                Console.WriteLine($"{favNum}, you guessed correctly! You're great at this!");
+               
+            }
+
+            else if (userGuess > favNum && userGuess <= 100) 
+            { 
+                Console.WriteLine($"{userGuess} isn't the right number! Way to high! Uh-oh!");
+            }
+             else if (userGuess < favNum && userGuess >= 0)
+            {
+                Console.WriteLine($"That's too low! {userGuess} isn't right!");
+                    }
+             else 
+            {
+                Console.WriteLine($"{userGuess}? That's not in the right number range! 0 to 100 only!");
+                    }
+            //EXERCISE TWO
+            Console.WriteLine("What is your favorite school subject? Please pick from Math, English, Science, PE, or Band.");
+            string userFavSub = Console.ReadLine();
+
+            switch (userFavSub)
+            {
+                case "Math":
+                    Console.WriteLine("Math is your favorite? You must be really smart!");
+                        break;
+                case "English":
+                    Console.WriteLine("English is so much fun! I bet you like reading, right?");
+                        break;
+                case "Science":
+                    Console.WriteLine("Science is so interesting! I really like learning about the POWERHOUSE OF THE CELL!");
+                    break;
+                case "PE":
+                    Console.WriteLine("PE? That is such a fun class!");
+                        break;
+                case "Band":
+                    Console.WriteLine("Band was always my favorite too! Especially marching band!");
+                        break;
+                default:
+                    Console.WriteLine("That wasn't one of the choices silly!");
+                    break;
+                   
+            }
         }
     }
 }
